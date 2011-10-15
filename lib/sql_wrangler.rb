b644e9bb2f61c5cda6428561fa4479dea3303c6c
Graeme Hill <graemekh@gmail.com>
2011-10-15 09:39:21 -0700

    killed some unnecessary whitespace on empty lines.  It doesn't really matter much, I mostly did it because emacs is currently highlighting it in red :).  I guess it's faster to parse this way...

--- lib/sql_wrangler.rb
+++ lib/sql_wrangler.rb
@@ -1,21 +1,21 @@
 require 'sqlite3'
 
 module SqlWrangler
-  
+
   class SqlConnection
-    
+
     def query(sql_string)
       Query.new self, sql_string
     end
-    
+
   end
-  
+
   class SqLiteConnection < SqlConnection
-    
+
     def initialize(db_path)
       @db = SQLite3::Database.new db_path
     end
-    
+
     def execute_sql(sql_string)
       @db.execute2(sql_string)
     end
@@ -27,44 +27,44 @@ module SqlWrangler
     def close
       @db.close
     end
-    
+
   end
-  
+
   class Query
-    
+
     attr_reader :sql_string
     attr_reader :groupings
     attr_reader :conn
-    
+
     def initialize(conn, sql_string)
       @conn = conn
       @sql_string = sql_string
       @groupings = []
     end
-    
+
     def execute
       raw_result = @conn.execute_sql(@sql_string)
       init_groups_for_execution raw_result[0]
       return format_query_result(raw_result)
     end
-    
+
     def format_query_result(raw_result)
       formatted_result = []
       columns_by_index = get_columns_by_index raw_result[0]
-      
+
       raw_result[1,raw_result.length-1].each do |raw_row|
         merge_row raw_row, @groupings, formatted_result, columns_by_index
       end
-      
+
       return formatted_result
     end
-    
+
     def get_columns_by_index columns
       columns_by_index = {}
       (0..columns.length-1).each { |i| columns_by_index[i] = columns[i] }
       return columns_by_index
     end
-    
+
     def init_groups_for_execution columns
       used_indexes = []
       @groupings.each do |group|
@@ -80,9 +80,9 @@ module SqlWrangler
         end
       end
     end
-    
+
     def merge_row(row, groups, grouped_data, columns_by_index)
-      
+
       if not @groupings.any?
         flat_row = {}
         columns_by_index.each { |index, column| flat_row[column] = row[index] }
@@ -112,7 +112,7 @@ module SqlWrangler
       end
 
     end
-    
+
     def get_existing_grouped_row grouped_vals, grouped_data
       grouped_data.each do |grouped_row|
         if not grouped_vals.keys.any? { |key| grouped_vals[key] != grouped_row[key] }
@@ -121,29 +121,29 @@ module SqlWrangler
       end
       return nil
     end
-    
+
     def group_by columns, options = {}
       new_grouping = QueryGrouping.new(options[:into], columns)
       new_grouping.level = @groupings.length
       @groupings << new_grouping
       return self
     end
-    
+
   end
-  
+
   class QueryGrouping
-    
+
     attr_reader :name
     attr_reader :columns
     attr_accessor :content_indexes
     attr_accessor :group_indexes
     attr_accessor :level
-    
+
     def initialize(name, columns)
       @name = name
       @columns = columns
     end
-    
+
   end
-  
+
 end
